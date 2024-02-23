9b0794c047f1e4b2e1f1660551f5e929b92f5f28
HilFerr <157055839+HilFerr@users.noreply.github.com>
2024-02-23 11:19:17 +0000

    Update and rename namesobrenome.cpp to Exercise2.cpp

---
+++ C++/Exercises/Exercise2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+// SIMPLE CODE THAT WILL SHOW THE USER THE LETTERS IN THE NAME & SIRNAME
+int main() {
+
+    // VARIABLES
+    string name;
+    string sirname;
+
+    cout << "What's your name? ";
+    cin >> name;
+
+    cout << "What's your sirname? ";
+    cin >> sirname;
+
+    // SHOWS THE USER THEIR NAME
+    cout << "\nHello dear " << name[0] << ". " << sirname << "." << endl;
+    cout << "With your sirname, " << sirname << ", we have " << sirname[0] << " as the first letter." << endl;
+    cout << "With your name, " << name << ", we have " << name[0] << " as the first letter." << endl;
+    cout << "So, you'll be called " << name[0] << sirname[0] << "." << endl;
+
+    return 0;
+}
